757eaeb7f5362a718eb7323d26c5e96ba9149099
pao-0019 <lr.prodriguez@mmdc.mcl.edu.ph>
2025-05-24 01:24:32 +0800

    Create CompensationDetails.java

---
+++ CompensationDetails.java
@@ -0,0 +1,74 @@
+public class CompensationDetails {
+    // Attributes
+    private double basicSalary;
+    private double riceSubsidy;
+    private double phoneAllowance;
+    private double clothingAllowance;
+    private double grossSemiMonthlyRate;
+    private double hourlyRate;
+
+    // Constructor
+    public CompensationDetails(double basicSalary, double riceSubsidy, double phoneAllowance, 
+                               double clothingAllowance, double grossSemiMonthlyRate, double hourlyRate) {
+        this.basicSalary = basicSalary;
+        this.riceSubsidy = riceSubsidy;
+        this.phoneAllowance = phoneAllowance;
+        this.clothingAllowance = clothingAllowance;
+        this.grossSemiMonthlyRate = grossSemiMonthlyRate;
+        this.hourlyRate = hourlyRate;
+    }
+
+    // Getter and Setter for basicSalary
+    public double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    // Getter and Setter for riceSubsidy
+    public double getRiceSubsidy() {
+        return riceSubsidy;
+    }
+
+    public void setRiceSubsidy(double riceSubsidy) {
+        this.riceSubsidy = riceSubsidy;
+    }
+
+    // Getter and Setter for phoneAllowance
+    public double getPhoneAllowance() {
+        return phoneAllowance;
+    }
+
+    public void setPhoneAllowance(double phoneAllowance) {
+        this.phoneAllowance = phoneAllowance;
+    }
+
+    // Getter and Setter for clothingAllowance
+    public double getClothingAllowance() {
+        return clothingAllowance;
+    }
+
+    public void setClothingAllowance(double clothingAllowance) {
+        this.clothingAllowance = clothingAllowance;
+    }
+
+    // Getter and Setter for grossSemiMonthlyRate
+    public double getGrossSemiMonthlyRate() {
+        return grossSemiMonthlyRate;
+    }
+
+    public void setGrossSemiMonthlyRate(double grossSemiMonthlyRate) {
+        this.grossSemiMonthlyRate = grossSemiMonthlyRate;
+    }
+
+    // Getter and Setter for hourlyRate
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+}
